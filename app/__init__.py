9f598c5a3ba0d3dc243cc7fa00d2c2a745d146c1
Silvi Claudia <silviclaudiawork@gmail.com>
2026-05-06 00:22:53 +0800

    Updated and working register page, tidy the whole structure for the project and easier to use, create routing as well for login

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-
 from config import Config
-
 
 db = SQLAlchemy()
 login_manager = LoginManager()
-login_manager.login_view = "main.index"
+login_manager.login_view = "auth.login"
 
 
 def create_app(config_class=Config):
@@ -17,9 +15,14 @@ def create_app(config_class=Config):
     db.init_app(app)
     login_manager.init_app(app)
 
-    from app import models  # noqa: F401
-    from app.routes import main
-
+    # Outside of main package
+    from app.main.models import User, StudySet, Flashcard
+    
+    #blueprint for dahsboard
+    from app.main.routes import main
     app.register_blueprint(main)
+
+    from app.auth.routes import auth
+    app.register_blueprint(auth)
 
     return app
