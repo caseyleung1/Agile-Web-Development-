6041871c3c34ddb33f6b249c7f7c9757f3ccb86d
caseyleung1 <caseyleung.au@gmail.com>
2026-05-06 11:54:31 +0800

    changed the location of the files, got rid of redundant files, easier location (standard location access) for flask to find

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,18 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def index():
     return render_template("index.html")
+
+@main.route('tests/analytics')
+@login_required
+def analytics():
+    totalcards = sum(len(s_set.flashcards) for s_set in current_user.study_sets)
+
+    #data for the visuals 
+    stats = {
+        'cards': totalcards, 
+        'accuracy': 89, 
+        'streak': 12, 
+        'chart_data': [50, 65, 78, 42, 61, 20, 40]
+    }
+
+    return render_template('analytics.html', **stats)
